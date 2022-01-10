71e598f498582fba8ae93e73ba1a6e690aff0299
Mjumbe Poe <mjumbewu@gmail.com>
2022-01-09 21:51:27 -0500

    Add pytest and jest test suites
    The pytest tests are static tests that check the structure of the folder, ensuring that the correct files exist. The jest tests on the other hand load the pages in a virtual browser and ensure that the page contents are correct.

---
+++ __tests__/test_correct_files.py
@@ -0,0 +1,68 @@
+import os
+import pathlib
+import re
+
+root_dir = pathlib.Path(__file__).parent.parent
+
+ADDED_FILES = os.environ.get('ADDED_FILES', '')
+NO_NEW_HTML_FILES_MESSAGE = '''
+Your PR should contain a new HTML file in the introductions/ folder, but does
+not appear to. Your new file should be named YOURNAME.html, where you replace
+YOURNAME with your actual name.
+'''
+NEW_HTML_FILE_CALLED_YOURNAME_MESSAGE = '''
+Your new HTML file should not literally be called {newfilename}.html. You should
+replace {newfilename} with your actual name.
+'''
+
+REMOVED_FILES = os.environ.get('REMOVED_FILES', 'htllo')
+REMOVED_FILES_MESSAGE = '''
+Your PR should not remove any files from the repository. Please restore any
+removed files. For example, if I were working from the command line I would do
+so like this:
+
+    git remote add upstream https://github.com/musa-611-spring-2022/musa-611-spring-2022-week1.git
+    git checkout upstream/main -- NAME FILES THAT WERE REMOVED HERE
+'''
+
+MODIFIED_FILES = os.environ.get('MODIFIED_FILES', '')
+MODIFIED_START_FILE_MESSAGE = '''
+Your PR must not modify the introductions/_start.html file. Please restore the
+version of _start.html in your repository. For example, if I were using the
+command line, I would add the original assignment repository as a "remote", and
+then check out the original _start.html file from there like this:
+
+    git remote add upstream https://github.com/musa-611-spring-2022/musa-611-spring-2022-week1.git
+    git checkout upstream/main -- introductions/_start.html
+'''
+
+
+def test_start_file_has_not_been_modified():
+    """Test that the _start.html file has not been modified"""
+
+    assert 'introductions/_start.html' not in MODIFIED_FILES, \
+           MODIFIED_START_FILE_MESSAGE
+
+
+def test_new_html_file_has_been_added():
+    """Test that a new HTML file has been added"""
+
+    html_file_pattern = re.compile(r'introductions/.*\.html')
+    assert html_file_pattern.search(ADDED_FILES) is not None, \
+           NO_NEW_HTML_FILES_MESSAGE
+
+
+def test_new_html_file_not_called_yourname():
+    """Test that there's not a new HTML file named YOURNAME.html"""
+
+    yourname_pattern = re.compile(r'introductions/(yourname)\.html',
+                                  re.IGNORECASE)
+    youname_file_name = yourname_pattern.search(ADDED_FILES)
+    assert youname_file_name is None, \
+           NEW_HTML_FILE_CALLED_YOURNAME_MESSAGE.format(newfilename=youname_file_name.groups(1)[0])
+
+
+def test_no_files_have_been_removed():
+    """Test that no files have been removed"""
+
+    assert len(REMOVED_FILES) == 0, REMOVED_FILES_MESSAGE
---
+++ __tests__/test_html_contents.js
@@ -0,0 +1,102 @@
+/* global describe, beforeAll, it, page, expect */
+
+const fs = require('fs');
+const path = require('path');
+
+const introductionsDir = path.join(__dirname, '/../introductions');
+const htmlFileNames = fs.readdirSync(introductionsDir).filter((fileName) => (
+  (fileName.slice(-5) === '.html')
+    && (fileName !== 'index.html')
+    && (fileName !== '_start.html')
+));
+
+htmlFileNames.forEach((fileName) => {
+  describe(`Introduction document ${fileName}`, () => {
+    beforeAll(async () => {
+      await page.goto(`http://localhost:8000/introductions/${fileName}`);
+    });
+
+    it('should not be titled with "YOUR NAME HERE"', async () => {
+      await expect(page.title()).resolves.not.toMatch(/YOUR NAME HERE/i);
+    });
+
+    it('should have an h1 with a name inside', async () => {
+      const nameH1 = await page.$('#identity > h1');
+      expect(nameH1).not.toBeNull();
+
+      const name = await nameH1.evaluate((node) => node.textContent);
+      expect(name).toBeTruthy();
+    });
+
+    function describeProfileLink(className, urlPrefix) {
+      describe(className + ' link', () => {
+        it('element should exist', async () => {
+          const link = await page.$(`.profiles .${className} a`);
+          expect(link).not.toBeNull();
+        });
+
+        it('should have an appropriate href URL', async () => {
+          const link = await page.$(`.profiles .${className} a`);
+          const href = link ? await link.evaluate((node) => node.href) : '';
+          expect(href).toMatch(new RegExp(urlPrefix + '.*', 'i'));
+        });
+
+        it('should have some content inside', async () => {
+          const link = await page.$(`.profiles .${className} a`);
+          const username = link ? await link.evaluate((node) => node.textContent) : '';
+          expect(username).toBeTruthy();
+        });
+      });
+    }
+
+    describeProfileLink('github-profile', 'https://www.github.com/');
+    describeProfileLink('slack-profile', 'https://musa509611.slack.com/team/');
+
+    function describeTechnologyImage(className, altMapping) {
+      describe(className + ' image', () => {
+        it('element should exist', async () => {
+          const image = await page.$(`.${className} img`);
+          expect(image).not.toBeNull();
+        });
+
+        it('should have a valid src', async () => {
+          const image = await page.$(`.${className} img`);
+
+          // src should start with "images/" and end with ".png"
+          const src = image ? await image.evaluate((node) => node.src) : '';
+          expect(src).toMatch(/images\/.*\.png/);
+        });
+
+        it('should have an alt value that matches the src', async () => {
+          const image = await page.$(`.${className} img`);
+          const src = image ? await image.evaluate((node) => node.src) : '';
+          const alt = image ? await image.evaluate((node) => node.alt) : '';
+          expect(src).toContain(altMapping[alt]);
+        });
+      });
+    }
+
+    describeTechnologyImage('operating-system', {
+      'Windows': 'images/windows.png',
+      'MacOS': 'images/macos.png',
+      'Linux': 'images/linux.png',
+      'Android': 'images/android.png',
+      'iOS': 'images/ios.png',
+      'ChromeOS': 'images/chromeos.png',
+    });
+    describeTechnologyImage('web-browser', {
+      'Google Chrome': 'images/chrome.png',
+      'Apple Safari': 'images/safari.png',
+      'Mozilla Firefox': 'images/firefox.png',
+      'Microsoft Edge': 'images/edge.png',
+      'Microsoft Exlorer': 'images/exlorer.png',
+    });
+
+    describe('interests-and-experiences section', () => {
+      it('should have at least two paragraphs', async () => {
+        const paragraphs = await page.$$('#interests-and-experiences p')
+        expect(paragraphs.length).toBeGreaterThanOrEqual(2)
+      })
+    });
+  });
+});
