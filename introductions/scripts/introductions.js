5d19b7ac512e806f3e4eb344f96ccb1a162d7ae9
Mjumbe Poe <mjumbewu@gmail.com>
2022-01-11 22:53:55 -0500

    Add map of home town

---
+++ introductions/scripts/introductions.js
@@ -0,0 +1,17 @@
+var map = L.map('home-town-map').setView(
+  homeTownCenter,
+  homeTownZoomLevel
+);
+
+var baseLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+})
+
+var marker = L.marker(homeTownCenter);
+
+baseLayer.addTo(map);
+marker.addTo(map);
