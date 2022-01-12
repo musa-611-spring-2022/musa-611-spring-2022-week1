ec8de79180582c99ff26638b92bdcba8c84c6fdd
Mjumbe Poe <mjumbewu@gmail.com>
2022-01-11 23:18:09 -0500

    Fix linting in introductions.js

--- introductions/scripts/introductions.js
+++ introductions/scripts/introductions.js
@@ -1,17 +1,19 @@
-var map = L.map('home-town-map').setView(
+/* global L, homeTownCenter, homeTownZoomLevel */
+
+const map = L.map('home-town-map').setView(
   homeTownCenter,
-  homeTownZoomLevel
+  homeTownZoomLevel,
 );
 
-var baseLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
-})
+const baseLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png',
+});
 
-var marker = L.marker(homeTownCenter);
+const marker = L.marker(homeTownCenter);
 
 baseLayer.addTo(map);
 marker.addTo(map);
